e71cad35363bae4623d187c5571b6e70b81d387a
Meelaay <elbaraka.amine@yahoo.com>
2020-03-24 10:35:22 +0100

    iteraion 4

--- LeKebab/Kebab.cs
+++ LeKebab/Kebab.cs
@@ -37,5 +37,49 @@ namespace LeKebab
 
             return true;
         }
+
+        public void AddSauce(string sauce)
+        {
+            _ingredients.Add(sauce); 
+        }
+
+        public static Kebab SansOignon(Kebab kebab)
+        {
+            foreach (var ingredient in kebab._ingredients)
+            {
+                if (ingredient == "Oignon")
+                {
+                    kebab._ingredients.Remove(ingredient);
+                    break;
+                }
+            }
+
+            return kebab;
+        }
+
+        public static Kebab DoubleCheese(Kebab kebab)
+        {
+            foreach (var ingredient in kebab._ingredients)
+            {
+                if (ingredient .Contains("Cheese") )
+                {
+                    kebab._ingredients.Add("Double " + ingredient);
+                    kebab._ingredients.Remove(ingredient);
+                    break;
+                }
+            }
+
+            return kebab;
+        }
+        /*
+         *Ajouter une methode renvoit un kebab nommée SansOignon et qui renvoit
+         * le kebab avec tous les ingrédients déjà configurés mais sans les oignons si
+         * il y en avait
+           
+           Ajouter une methode renvoit un kebab nommée SupplémentFromage et qui renvoit
+           le kebab avec tous les ingrédients déjà configurés mais pour chaque ingrédient
+           fromage ca le double
+         *
+         */
     }
 }
