a2174bba6e9c6403e85278c1856c73b7fc22a884
Meelaay <elbaraka.amine@yahoo.com>
2020-03-24 10:08:17 +0100

    iteration 1

---
+++ LeKebab/Kebab.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace LeKebab
+{
+    class Kebab
+    {
+        private List<string> _ingredients = new List<string>();
+
+        public Kebab(List<string> ingredients)
+        {
+            _ingredients = ingredients;
+        }
+
+        public bool IsVegan()
+        {
+            foreach (var ingredient in _ingredients)
+            {
+                if (ingredient != "tomate" || ingredient != "onion")
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
